b89e8eb2a8590e317a69327638f5ea89a82c0b83
Paul Bostoen <bostoenpaul@gmail.com>
2023-04-13 21:53:30 +0200

    Branche_Paul_PDF_V1

--- src/Command.java
+++ src/Command.java
@@ -6,11 +6,9 @@
  * A command currently consists of two strings: a command word and a second
  * word (for example, if the command was "take map", then the two strings
  * obviously are "take" and "map").
- * 
  * The way this is used is: Commands are already checked for being valid
  * command words. If the user entered an invalid command (a word that is not
  * known) then the command word is <null>.
- *
  * If the command had only one word, then the second word is <null>.
  * 
  * @author  Michael Kölling and David J. Barnes
